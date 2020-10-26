a8fae54710a584217d62ab2a0458239099206438
Ramiro Vera <ramirovera@apexar.com>
2020-10-26 12:37:54 -0300

    .Se agrega informacion sobre la clase Block 	*Se agrega clase soporte Vector 	*Se agrega Test-Block.cpp para testear la clase 	*Se modifica el makefile para agregar las modificaciones

--- Src/Block.h
+++ Src/Block.h
@@ -1,17 +1,46 @@
-/*
- * Block.h
- *
- *  Created on: 25 oct. 2020
- *      Author: Ramiro
- */
+//Archivo fuente header clase Block / AlgoBlock del tp0 para la materia 9512 Algoritmos y Programación 2.
 
-#ifndef BLOCK_H_
-#define BLOCK_H_
+#include <string>
 
-class Block {
-public:
-	Block();
-	virtual ~Block();
+enum TiposHash {
+	clavehash256,
+	clavefirma,
 };
 
-#endif /* BLOCK_H_ */
+using namespace std;
+
+const string::size_type LargoHashEstandar = 64;
+const string::size_type LargoHashFirma    = 40;	// Hash Pública de la Cuenta
+
+class Block {
+    // Métodos
+	private:
+		// Atributos Seccion Header
+		string pre_block;
+		string txns_hash;
+		string bits;
+		string nonce;
+		// Atributos Seccion Body
+		// Body *body;	// Puntero a un objeto clase body.
+		bool CheckPreBlock( string valor );
+		int CheckHexa( string value );
+    public:
+        // Constructores
+        Block();
+        // Destructor
+        ~Block();
+		// Getters
+        int getCantTransacciones();
+		string getpre_block();
+		string gettxns_hash();
+		string getbits();
+		string getnonce();
+		// Setters
+		bool setpre_block( string valor );
+		bool settxns_hash( string valor );
+		bool setbits( string valorbits );
+		bool setnonce( string valor );
+		// Métodos
+		void RecalculoHash();
+		bool CheckHash( string valor, TiposHash Tipo = clavehash256 );
+};
